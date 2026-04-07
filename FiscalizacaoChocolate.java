4dcd991fe4edc4d8d2ab21a049cc9e65c0b7edac
Vinicius Lazaroti <viniciuslazarotti@gmail.com>
2026-04-07 19:14:09 -0300

    FiscalizacaoChocolate.java

--- FiscalizacaoChocolate.java
+++ FiscalizacaoChocolate.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class choco {
+public class FiscalizacaoChocolate {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
